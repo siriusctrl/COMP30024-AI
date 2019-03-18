83022a582a7f25543217ef24eddbd076cd8faa22
Shouyin <7000kl@163.com>
2019-03-18 16:42:50 +1100

    Changed to Node

---
+++ Node.py
@@ -0,0 +1,10 @@
+import math
+
+class Node:
+
+    def __init__(self, pre: Node) -> None:
+        self.pre = pre
+        self.next = []
+        self.coordinate = None
+        # TODO: finish the build of the tree
+    
--- tree.py
+++
@@ -1,8 +0,0 @@
-import math
-
-class tree:
-
-    def __init__(self, pre:tree) -> None:
-        self.pre = pre
-        self.next = None
-        # TODO: finish the build of the tree
