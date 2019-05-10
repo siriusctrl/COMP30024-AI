bbab20a3d99019a1a71497f6b9cfc56a1ec5e3d6
Shouyin <7000kl@163.com>
2019-05-10 14:22:22 +1000

    Auto stash before rebase of "origin/RL-morry"

--- Project2/HardCode/utils.py
+++ Project2/HardCode/utils.py
@@ -115,7 +115,7 @@ def cal_all(current_board, next_bor, colour, colour_e, colour_p, action, arrange
 
         rew = 0
 
-        d_heurii = cal_rheu(current_board, next_bor, colour, -1)
+        d_heurii = cal_rheu(current_board, next_bor, colour, colour_e[colour])
         
         if exit_this:
             rew += config.EXIT_RW
@@ -159,8 +159,7 @@ def heuristic(players, colour, player_exit):
         tmp_h.append(config.COST[colour][p])
 
     tmp_h.sort()
-    if player_exit == -1:
-        return sum(tmp_h)
+    
     if len(players) + player_exit >= 4:
         h = sum(tmp_h[:4 - player_exit])
     else:
